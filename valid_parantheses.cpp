585a2e6c4fb44532aafecd2011ea5d5611bb2a8c
abhishekyd300 <abhishekyd300@gmail.com>
2025-08-20 22:24:44 +0530

    Add files via upload

---
+++ valid_parantheses.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isValid(string str) {
+      stack<char> st;
+
+      for(int i=0;i<str.size();i++){
+        if(str[i]=='('||str[i]=='{'||str[i]=='['){
+            st.push(str[i]);
+        }
+        else{
+            if(st.size()==0){
+                return false;        //stack empty--
+            }
+            if((st.top()=='('&&str[i]==')')||
+                (st.top()=='{'&&str[i]=='}')||
+                (st.top()=='['&&str[i]==']'))
+            {
+                st.pop();
+            } 
+            else{
+            return false;}   
+        }    
+        }
+        return st.size()==0;
+}
+
+int main(){
+    string str="({}[])";
+     if (isValid(str))
+        cout << "valid" << endl;
+    else
+        cout << "invalid" << endl;
+
+    return 0;
+
+}
